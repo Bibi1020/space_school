1ccb14a82588b5ee6ff7b00408d5b5935bf468eb
Isneybis Arvelo <isneybis1914@gmail.com>
2020-11-17 12:19:24 +0100

    cambios

--- lib/space_school/movements.rb
+++ lib/space_school/movements.rb
@@ -1,17 +1,15 @@
 module Movements
     
-    def right
-      x = 9
-      self.position[x][1] += 1
-
-    end
+  def right
+    x = 9
+    self.position[x][1] += 1
+  end
       
     
-    def left
-     x = 9
-     self.position[x][1] -= 1
-
-    end
+  def left
+    x = 9
+    self.position[x][1] -= 1
+  end
 
   def down
     self.position[0] += 1
--- lib/space_school/ship.rb
+++ lib/space_school/ship.rb
@@ -1,16 +1,19 @@
 require_relative 'movements'
 
 class Ship
+ 
+  attr_accessor :position, :shape
 
-    attr_accessor :position, :shape
-
-    def initialize 
-        @position = [9,5]
-        @shape = "00"
-    end
-
-    def show_ship
-        @shape
-    end
+  def initialize 
+    @position = [9,5] 
+    @shape = "00" 
+  end 
+    
+  def show_ship 
+    @shape 
+  end
+  
+    
 
 end
+    
