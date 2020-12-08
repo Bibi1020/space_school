c031a656360f6d18a80e6bc877dacad30b55ee81
Nathaly Torrealba <ntorrealba.s@gmail.com>
2020-12-07 20:36:58 -0400

    rubocop fixed offenses

--- lib/space_school.rb
+++ lib/space_school.rb
@@ -1,12 +1,13 @@
-require_relative "space_school/version"
-require "rainbow"
+# frozen_string_literal: true
+
+require_relative 'space_school/version'
+require 'rainbow'
 require_relative 'space_school/welcome'
 require_relative 'space_school/game'
 
-
 module SpaceSchool
   LIMIT = 14
-  #class Error < StandardError; end
+  # class Error < StandardError; end
   # Your code goes here...
 end
 
--- lib/space_school/utils.rb
+++ lib/space_school/utils.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Manejo el input del usuario. No es necesario entender por ahora que es lo que está pasando aquí ;)
 
 module Utils
@@ -15,13 +17,12 @@ module Utils
   # Return::
   # * _Integer_: ASCII code of the last key pressed, or nil if none
   def self.get_key
-    if @use_stty
+    return char if @use_stty
       system('stty raw -echo') # => Raw mode, no echo
       char = (STDIN.read_nonblock(1).ord rescue nil)
       system('stty -raw echo') # => Reset terminal mode
-      return char
     else
-      return Win32API.new('crtdll', '_kbhit', [ ], 'I').Call.zero? ? nil : Win32API.new('crtdll', '_getch', [ ], 'L').Call
+      return Win32API.new('crtdll', '_kbhit', [], 'I').Call.zero? ? nil : Win32API.new('crtdll', '_getch', [], 'L').Call
     end
   end
 end
