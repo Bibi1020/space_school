b79d94819a7fa892e0cc4419a128b0ef0f283d2a
Mireille Perez <mireperez8@gmail.com>
2020-12-15 23:06:10 -0400

    creation of space_spec with statement

---
+++ spec/space_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec.describe Space do
+  it 'if there is a winner ship up' do
+
+  end
+
+  it 'when generate meteors replace in matrix' do
+
+  end
+
+  it 'if a meteor in his position[0] is 9, replace for blank space' do
+
+  end
+
+  it 'replace ship in matrix' do
+
+  end
+
+  it 'verify that meteor and ship are not in the same position' do
+
+  end
+end
