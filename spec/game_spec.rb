df4f39046fe3aa30862615eeb5de2fe149d48655
Isneybis Arvelo <isneybis1914@gmail.com>
2020-12-16 04:43:29 +0100

    spec game

---
+++ spec/game_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe Game do
+  it 'if the entry in a or d the ship moves' do
+
+  end
+
+  it 'if the input is x game over' do
+
+  end
+
+  it 'when frames increase speed up do' do
+
+  end
+
+  it 'when frames 200 equal winner' do
+
+  end
+
+end
